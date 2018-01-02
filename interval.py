36d778f2e0a21d668097b919269e734e7bc65a75
Ashwin <ashwinvargh@gmail.com>
2018-01-02 12:57:25 -0500

    Started working with datetime

---
+++ interval.py
@@ -0,0 +1,4 @@
+import datetime
+
+now = datetime.datetime.utcnow()
+# add 5 hours to whatever time you see on your clock for testing
--- quickstart.py
+++ quickstart.py
@@ -6,10 +6,9 @@ from apiclient import discovery
 from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
-
 import datetime
 
-try:
+try: # here for older python versions (argsparse imported on 2.7 and onwards)
     import argparse
     flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
 except ImportError:
@@ -17,9 +16,10 @@ except ImportError:
 
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/calendar-python-quickstart.json
-SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
+SCOPES = 'https://www.googleapis.com/auth/calendar' # limit to read/write only.
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'Google Calendar API Python Quickstart'
+CALENDAR_ID = 'l28ku7sivedk160fdgf0ttnka0@group.calendar.google.com'
 
 
 def get_credentials():
@@ -38,13 +38,13 @@ def get_credentials():
     credential_path = os.path.join(credential_dir,
                                    'calendar-python-quickstart.json')
 
-    store = Storage(credential_path)
-    credentials = store.get()
+    store = Storage(credential_path) # Storage class stores and retreives a single Credentials object
+    credentials = store.get() # holds refresh and access tokens
     if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES) # flow object (process -> credentials)
         flow.user_agent = APPLICATION_NAME
         if flags:
-            credentials = tools.run_flow(flow, store, flags)
+            credentials = tools.run_flow(flow, store, flags) # authorization flow in default browser
         else: # Needed only for compatibility with Python 2.6
             credentials = tools.run(flow, store)
         print('Storing credentials to ' + credential_path)
@@ -56,21 +56,39 @@ def main():
     Creates a Google Calendar API service object and outputs a list of the next
     10 events on the user's calendar.
     """
+
+    event = {
+        'summary': 'Google I/O 2018',
+        'location': '800 Howard St., San Francisco, CA 94103',
+        'description': 'A chance to hear more about Google\'s developer products.',
+        'start': {
+            'dateTime': '2018-01-02T21:00:00-05:00',
+            'timeZone': 'America/Los_Angeles',
+        },
+        'end': {
+         'dateTime': '2018-01-02T22:00:00-05:00',
+         'timeZone': 'America/Los_Angeles',
+        }
+    }
+
     credentials = get_credentials()
-    http = credentials.authorize(httplib2.Http())
-    service = discovery.build('calendar', 'v3', http=http)
+    http = credentials.authorize(httplib2.Http()) # adds credentials headers to http object
+    service = discovery.build('calendar', 'v3', http=http) # creates a discovery service object
 
     now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
-    eventsResult = service.events().list(
-        calendarId='primary', timeMin=now, maxResults=10, singleEvents=True,
-        orderBy='startTime').execute()
-    events = eventsResult.get('items', [])
+    print('Getting the upcoming 10 events...')
+    service.events().insert(calendarId=CALENDAR_ID, body=event).execute()
+    eventsResult = service.events().list( # creates an HttpRequest object
+        calendarId=CALENDAR_ID,
+        timeMin=now, maxResults=10, singleEvents=True,
+        orderBy='startTime').execute() #.execute() gets the response
+    
+    events = eventsResult.get('items', []) # return empty if items doesn't exist.
 
     if not events:
         print('No upcoming events found.')
     for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
+        start = event['start'].get('dateTime', event['start'].get('date')) # the DNE case is for all-day events
         print(start, event['summary'])
 
 
